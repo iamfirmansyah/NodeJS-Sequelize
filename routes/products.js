07102f98718cf42462900723fa93d2f6b4f71e17
iamfirmansyah <imam.firmansyah@yesdok.com>
2021-12-07 21:05:51 +0700

    add update function

--- routes/products.js
+++ routes/products.js
@@ -28,8 +28,40 @@ router.post('/', async(req, res) => {
     })
 
     res.json(product)
+})
 
+router.put('/:id', async(req, res) => {
+    const id = req.params.id
 
+    let product = await Product.findByPk(id)
+
+    if (!product) {
+        return res.status(401).json({
+            message: `Product where id ${id} not found`
+        })
+    }
+
+    const schema = {
+        name: 'string|optional',
+        brand: 'string|optional',
+        description: 'string|optional',
+    }
+
+    const validate = v.validate(req.body, schema)
+
+    if (validate.length) {
+        return res
+            .status(400)
+            .json(validate)
+    }
+
+    product = await product.update({
+        name: req.body.name,
+        brand: req.body.brand,
+        description: req.body.description
+    })
+
+    res.send(product)
 })
 
 module.exports = router
